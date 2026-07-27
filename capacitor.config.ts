8d9367f6a2712f32ce1365b4dd342631b0ec79d8
IgorTientcheu <tientcheuigorcarel@gmail.com>
2026-07-27 15:18:54 +0100

    ajustement de la responsivite

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alertproche.app',
+  appName: 'AlertProche',
+  webDir: 'dist/alert-proche/browser'
+};
+
+export default config;
