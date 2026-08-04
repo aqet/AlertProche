0a604a19c48b171c82863f52cb88b52560f2f23e
IgorTientcheu <tientcheuigorcarel@gmail.com>
2026-08-04 16:57:03 +0100

    les notifif push passe bien now

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,21 +1,22 @@
 import { Component, computed, HostListener, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { AuthService } from '../../core/services/auth.service';
 import { ThemeService } from '../../core/services/theme.service';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, NgIf],
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-  isOpen=false;
+  isOpen = false;
   menuOpen = signal(false);
   scrolled = signal(false);
-
+  IsMobile: boolean = false;
   isAuth = computed(() => this.auth.isAuthenticated());
   user = computed(() => this.auth.currentUser());
   isDark = computed(() => this.theme.currentTheme() === 'dark');
@@ -24,14 +25,30 @@ export class NavbarComponent {
     return u?.role === 'Moderateur' || u?.role === 'Admin';
   });
 
-  constructor(public auth: AuthService, public theme: ThemeService) {}
+  constructor(
+    public auth: AuthService,
+    public theme: ThemeService,
+  ) {
+    if (Capacitor.isNativePlatform()) {
+      this.IsMobile=false
+    }
+    console.log(this.IsMobile);
+    
+  }
 
   @HostListener('window:scroll')
   onScroll() {
     this.scrolled.set(window.scrollY > 20);
   }
 
-  toggleMenu() { this.isOpen=!this.isOpen }
-  closeMenu() { this.isOpen=false }
-  logout() { this.auth.logout(); this.closeMenu(); }
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
+  closeMenu() {
+    this.isOpen = false;
+  }
+  logout() {
+    this.auth.logout();
+    this.closeMenu();
+  }
 }
